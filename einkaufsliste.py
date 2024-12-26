f246d0e1febde46b3d0e8ea9b5251bec07558b38
Jonas H <hauber.sonja@gmx.de>
2024-12-26 09:21:33 +0100

    first testing implemented, einkaufsliste created, untested

---
+++ einkaufsliste.py
@@ -0,0 +1,38 @@
+from zutat import Zutat
+
+class Einkaufsliste:
+    def __init__(self):
+        """
+        Erstellt eine leere Einkaufsliste.
+        """
+        self.zutaten = {}
+
+    def add_zutat(self, zutat):
+        """
+        Fügt eine Zutat zur Einkaufsliste hinzu. Wenn die Zutat bereits vorhanden ist, wird die Menge addiert.
+        
+        :param zutat: Ein Zutat-Objekt.
+        """
+        key = (zutat.name, zutat.einheit)  # Schlüssel: (Name, Einheit)
+        if key in self.zutaten:
+            self.zutaten[key].menge += zutat.menge
+        else:
+            self.zutaten[key] = Zutat(zutat.name, zutat.menge, zutat.einheit, zutat.beschreibung)
+
+    def zusammenfassen(self, rezepte):
+        """
+        Fasst die Zutaten aus mehreren Rezepten zusammen.
+        
+        :param rezepte: Eine Liste von Rezept-Objekten.
+        """
+        for rezept in rezepte:
+            for zutat in rezept.zutaten:
+                self.add_zutat(zutat)
+
+    def anzeigen(self):
+        """
+        Gibt die Einkaufsliste sortiert nach Zutatenname aus.
+        """
+        print("Einkaufsliste:")
+        for zutat in sorted(self.zutaten.values(), key=lambda z: z.name):
+            print(f"{zutat.menge:.2f} {zutat.einheit} {zutat.name}")
